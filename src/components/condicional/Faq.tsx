1401184f16c726579647c4b4471a1ccc792defa2
Weddly <weddly@hotmail.com>
2023-05-05 22:49:56 -0300

    Aula #3 - Renderização condicional com comunicação direta e indireta

--- src/components/condicional/Faq.tsx
+++ src/components/condicional/Faq.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Pergunta from "./Pergunta";
 
 export default function Faq(){
+    const [ativo, setAtivo] = useState<number>(0)
+
+    function alternarVisibilidade(indice: number) {
+        if(indice === ativo) {
+            setAtivo(-1)
+        } else {
+            setAtivo(indice)
+        }
+    }
+
     return(
         <div className="flex flex-col gap-2 w-[90%] md:w-3/4">
-            <Pergunta texto="O que o pato disse para a pata?" resposta="Vem Quá!" />
-            <Pergunta texto="Por que o menino estava falando ao telefone deitado?" resposta="Para não cair a ligação." />
-            <Pergunta texto="Qual é a fórmula da água benta?" resposta="H Deus O!" />
-            <Pergunta texto="Qual é a cidade brasileira que não tem táxi?" resposta="Uberlândia!" />
+            <Pergunta aberta={ativo === 0} indice={0} alternarVisibilidade={alternarVisibilidade} texto="O que o pato disse para a pata?" resposta="Vem Quá!" />
+            <Pergunta aberta={ativo === 1} indice={1} alternarVisibilidade={alternarVisibilidade} texto="Por que o menino estava falando ao telefone deitado?" resposta="Para não cair a ligação." />
+            <Pergunta aberta={ativo === 2} indice={2} alternarVisibilidade={alternarVisibilidade} texto="Qual é a fórmula da água benta?" resposta="H Deus O!" />
+            <Pergunta aberta={ativo === 3} indice={3} alternarVisibilidade={alternarVisibilidade} texto="Qual é a cidade brasileira que não tem táxi?" resposta="Uberlândia!" />
         </div>
     )
 }
--- src/components/condicional/Pergunta.tsx
+++ src/components/condicional/Pergunta.tsx
@@ -4,8 +4,11 @@ import { IconCaretDown, IconCaretUp } from "@tabler/icons-react";
 import IfElse from "./IfElse";
 
 interface PerguntaProps{
+    indice: number
     texto: string
     resposta: string
+    aberta: boolean
+    alternarVisibilidade: (indice: number) => void
 }
 
 export default function Pergunta(props: PerguntaProps) {
@@ -16,11 +19,11 @@ export default function Pergunta(props: PerguntaProps) {
         <div className={`border border-zinc-600 rounded-md overflow-hidden
         `}>
             <div className="bg-zinc-800 p-5  cursor-pointer select-none flex justify-between"
-                 onClick={() => setAberta(!aberta)}
+                 onClick={() => props.alternarVisibilidade(props.indice)}
             >
                 <span>{props.texto}</span>
 
-                <IfElse teste={aberta}>
+                <IfElse teste={props.aberta}>
                     <IconCaretUp />
                     <IconCaretDown />
                 </IfElse>
@@ -29,7 +32,7 @@ export default function Pergunta(props: PerguntaProps) {
                 
             </div>
 
-            <If teste={aberta}>
+            <If teste={props.aberta}>
                 <div className="p-5" >
                     {props.resposta}
                 </div>
